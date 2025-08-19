c6095fe22e63f79163a738804802da41a3c1cb54
pecavalc <pecavalc@student.42berlin.de>
2025-08-19 14:31:12 +0200

    New sort.c, including sort_two and sort_three

---
+++ srcs/sort.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/19 14:02:36 by pecavalc          #+#    #+#             */
+/*   Updated: 2025/08/19 14:25:25 by pecavalc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	sort_two_numbers(t_stack *a, t_stack *b)
+{
+	if (!a || !b || a->size != 2 || b->size !=2)
+	{
+		free_stacks(&a, &b);
+		print_error_and_exit();
+	}
+	if (get_from_stack(0, a) > get_from_stack(1, a))
+		sa(a, b);
+	return (0);
+}
+
+void	sort_three_numbers(t_stack *a, t_stack *b)
+{
+	if (!a || !b || a->size != 3 || b->size != 3)
+	{
+		free_stacks(&a, &b);
+		print_error_and_exit();
+	}
+	if (get_from_stack(0, a) > get_from_stack(1, a) &&
+		get_from_stack(0, a) > get_from_stack(2, a))
+		ra(a);
+	else if (get_from_stack(1, a) > get_from_stack(0, a) &&
+			 get_from_stack(1, a) > get_from_stack(2, a))
+		rra(a);
+	if (get_from_stack(0, a) > get_from_stack(1, a))
+		sa(a, b);
+}
